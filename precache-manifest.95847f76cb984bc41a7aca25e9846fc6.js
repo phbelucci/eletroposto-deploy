4492ab12c3f16f676919d71b8379fb15a52d4b9f
phbelucci <phbelucci@gmail.com>
2020-09-08 01:34:58 -0300

    Updates

---
+++ precache-manifest.95847f76cb984bc41a7aca25e9846fc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4513108d29de3aa809fceb48458051bf",
+    "url": "/eletroposto-deploy/index.html"
+  },
+  {
+    "revision": "bda9e4e4cb13ce801cca",
+    "url": "/eletroposto-deploy/static/css/main.f24fa79d.chunk.css"
+  },
+  {
+    "revision": "e861b0eda540e1ecbeec",
+    "url": "/eletroposto-deploy/static/js/2.d6360606.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/eletroposto-deploy/static/js/2.d6360606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda9e4e4cb13ce801cca",
+    "url": "/eletroposto-deploy/static/js/main.4fd7ee5b.chunk.js"
+  },
+  {
+    "revision": "8ca20055763a59db61b3",
+    "url": "/eletroposto-deploy/static/js/runtime-main.6b12ddd8.js"
+  },
+  {
+    "revision": "c6d5babe6119e8462cdeba14e90bf702",
+    "url": "/eletroposto-deploy/static/media/reciclar.c6d5babe.svg"
+  },
+  {
+    "revision": "b6b1d1e31903858b17e2b9a3c8235b2d",
+    "url": "/eletroposto-deploy/static/media/star.b6b1d1e3.png"
+  }
+]);
